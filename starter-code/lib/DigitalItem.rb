888fff79dcc1a1b6cc638ad57943d232165b89c4
katiosity <lovan.kathy@gmail.com>
2016-03-02 18:38:56 -0800

    added item ship price method

--- starter-code/lib/DigitalItem.rb
+++ starter-code/lib/DigitalItem.rb
@@ -1,1 +1,25 @@
 # You are on your own... good luck.
+require_relative 'Item.rb'
+
+class Digital < Item
+	attr_reader :quantity
+	# @@amount_sold = 0
+
+ 	def initialize(name, price)
+	    super(name,price)
+	    @quantity = 1
+	    @weight = -1
+  	end
+
+  	def sell
+  		true
+  	end
+
+  	def stock
+  		true
+  	end
+
+  	def ship_price
+  		false
+  	end
+end
--- starter-code/spec/digitalitem_spec.rb
+++ starter-code/spec/digitalitem_spec.rb
@@ -1,1 +1,37 @@
 # You are on your own... good luck.
+require_relative 'spec_helper'
+require_relative '../lib/DigitalItem'
+
+describe Digital do
+
+	before(:context) do
+		@digital = Digital.new("Cool Movie", 25)
+	end
+
+	describe "initialization" do
+
+		it "should be an instance of a digital item" do
+			expect(@digital).to be_kind_of(Digital)
+		end
+
+		it "should be extended from Item" do
+			expect(@digital).to be_kind_of(Item)
+		end
+	end
+
+	describe "method" do
+
+		it "ship price should return false" do
+			 expect(@digital.ship_price).to eq(false)
+		end
+
+		it "should sell" do
+			expect(@digital.sell).to eq(true)
+		end
+
+		#if sold quantity should remain at 1
+		it "should maintain quantity of 1 even if sold" do
+			expect(@digital.quantity).to eq(1)
+		end
+	end
+end
