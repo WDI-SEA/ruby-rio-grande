5417e48f0ea01ce31c5014930d45d91053ed4a37
katiosity <lovan.kathy@gmail.com>
2016-03-02 19:01:01 -0800

    added solutions for song and ebook

--- starter-code/spec/ebook_spec.rb
+++ starter-code/spec/ebook_spec.rb
@@ -1,1 +1,49 @@
 # You are on your own... good luck.
+require_relative 'spec_helper'
+require_relative '../lib/EBook'
+
+describe EBook do 
+	
+	before(:context) do
+		@ebook = EBook.new("My EBook", 20, 700, "Me")
+	end
+
+	#check initialization
+	describe "initialization" do
+
+		it "should be an instance of a digital item" do
+			expect(@ebook).to be_kind_of(Digital)
+		end
+
+		it "should be an instance of an ebook" do
+			expect(@ebook).to be_kind_of(EBook)
+		end
+
+		it "should assign a name" do
+			expect(@ebook.name).to eq("My EBook")
+		end
+
+		it "should assign assign pages" do
+			expect(@ebook.pages).to eq(700)
+		end
+
+		it "should assign assign an author" do
+			expect(@ebook.author).to eq("Me")
+		end
+	end
+
+
+	#check getters and setters
+	describe "getters and setters" do
+
+		it "assigns and reads the pages" do
+			@ebook.pages = 40000
+			expect(@ebook.pages).to eq(40000)
+		end
+
+		it "assigns and reads the author" do
+			@ebook.author = "Awesomeness"
+			expect(@ebook.author).to eq("Awesomeness")
+		end
+	end
+end
--- starter-code/spec/song_spec.rb
+++ starter-code/spec/song_spec.rb
@@ -1,1 +1,52 @@
 # You are on your own... good luck.
+require_relative 'spec_helper'
+require_relative '../lib/Song'
+
+describe Song do 
+	
+	before(:context) do
+		@song = Song.new("Coding is cool", 500, 800, "Me")
+	end
+
+	#check initialization
+	describe "initialization" do
+
+		it "should be an instance of a digital item" do
+			expect(@song).to be_kind_of(Digital)
+		end
+
+		it "should be an instance of a song" do
+			expect(@song).to be_kind_of(Song)
+		end
+
+		it "should assign a name" do
+			expect(@song.name).to eq("Coding is cool")
+		end
+
+		it "should assign price" do
+			expect(@song.price).to eq(500)
+		end
+
+		it "should assign run time" do
+			expect(@song.run_time).to eq(800)
+		end
+
+		it "should assign artist" do
+			expect(@song.artist).to eq("Me")
+		end
+	end
+
+	#check getters and setters
+	describe "getters and setters" do
+
+		it "assigns and reads the run_time" do
+			@song.run_time = 10
+			expect(@song.run_time).to eq(10)
+		end
+
+		it "assigns and reads the artist" do
+			@song.artist = "GA"
+			expect(@song.artist).to eq("GA")
+		end
+	end
+end
