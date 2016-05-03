1cd1fecf07ac408d251a72779bee9372624505bd
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-03 16:43:17 -0700

    initial commit

--- starter-code/spec/bluray_spec.rb
+++ starter-code/spec/bluray_spec.rb
@@ -4,9 +4,13 @@ require_relative '../lib/Bluray'
 describe Bluray do
 
   before(:context) do
-    #initialize item
+    @bluray = Bluray.new()
   end
 
+  describe Initialization do
+  	it "is an instance of the Bluray class" do
+  		expect(@bluray).to be_instance_of(Bluray)
+  end
   #check initialization
   #check that it is an extended from Item
   #check that it is an instance of Bluray
