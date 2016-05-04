97fb032ace4f002bcee17c2620541821ecfa5377
Paul <pacalabre@gmail.com>
2016-05-03 19:50:02 -0700

    finished songs

--- starter-code/lib/Song.rb
+++ starter-code/lib/Song.rb
@@ -1,1 +1,13 @@
-# You are on your own... good luck.
+require_relative 'Digitalitem.rb'
+
+class Song < Digitalitem
+
+  attr_accessor :run_time, :artist
+
+  def initialize run_time, artist
+    super(name,price)
+    @run_time = run_time
+    @artist = artist
+  end
+
+end
--- starter-code/spec/song_spec.rb
+++ starter-code/spec/song_spec.rb
@@ -1,1 +1,25 @@
-# You are on your own... good luck.
+require_relative 'spec_helper'
+require_relative '../lib/Song'
+
+describe Song do
+
+  before(:context) do
+    @song = Song.new("5 mins", "Radiohead")
+  end
+
+  describe "Initialization" do
+    it "is extended from Digitalitem" do
+      expect(@song).to be_kind_of(Digitalitem)
+    end
+    it "is an instance of Song" do
+      expect(@song).to be_instance_of(Song)
+    end
+    it "should have an run time of 5 mins" do
+      expect(@song.run_time).to eq("5 mins")
+    end
+    it "should have an artist of Radiohead" do
+      expect(@song.artist).to eq("Radiohead")
+    end
+  end
+
+end
