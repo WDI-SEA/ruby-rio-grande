dffa0c453392ba9ef8f5474463488a48aff8c65a
Paul <pacalabre@gmail.com>
2016-05-03 18:35:42 -0700

    bluray section done

--- starter-code/spec/bluray_spec.rb
+++ starter-code/spec/bluray_spec.rb
@@ -4,12 +4,24 @@ require_relative '../lib/Bluray'
 describe Bluray do
 
   before(:context) do
-    #initialize item
+     @bluray = Bluray.new("80 mins", "RZA", "RZA")
   end
 
   #check initialization
   #check that it is an extended from Item
   #check that it is an instance of Bluray
   #check getters and setters
+
+  describe "Initialization" do
+    it "is extended from Item" do
+      expect(@bluray).to be_kind_of(Item)
+    end
+    it "is an instance of Bluray" do
+      expect(@bluray).to be_instance_of(Bluray)
+    end
+    it "should have an run time of 80 mins" do
+      expect(@bluray.run_time).to eq("80 mins")
+    end
+  end
 
 end
