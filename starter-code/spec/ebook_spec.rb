23e414ca9d400405f41b2382e8d81fb7db63199a
Paul <pacalabre@gmail.com>
2016-05-03 19:22:31 -0700

    added ebooks

--- starter-code/spec/ebook_spec.rb
+++ starter-code/spec/ebook_spec.rb
@@ -1,1 +1,22 @@
-# You are on your own... good luck.
+require_relative 'spec_helper'
+require_relative '../lib/EBook'
+
+describe EBook do
+
+  before(:context) do
+    @ebook = EBook.new(200, "Stephen King")
+  end
+
+  describe "Initialization" do
+    it "is extended from Digitalitem" do
+      expect(@ebook).to be_kind_of(Digitalitem)
+    end
+    it "is an instance of Books" do
+      expect(@ebook).to be_instance_of(EBook)
+    end
+    it "should have an Author of Stephen King" do
+      expect(@egbook.author).to eq("Stephen King")
+    end
+  end
+
+end
