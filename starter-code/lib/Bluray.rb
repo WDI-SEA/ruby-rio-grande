55637791f7600356af7449ee69c160c77e017552
mattsluis <Sluismatthew@gmail.com>
2016-05-03 19:46:15 -0700

    book works

--- starter-code/lib/Bluray.rb
+++ starter-code/lib/Bluray.rb
@@ -1,3 +1,8 @@
 require_relative 'Item.rb'
 class Bluray < Item
+
+  def initialize name, price
+    super(name, price)
+  end
+  
 end
--- starter-code/lib/Book.rb
+++ starter-code/lib/Book.rb
@@ -1,7 +1,16 @@
-require_relative 'Item.rb'
-class Book < Item
 
-  def initialize name, price
-    super(name,price)
+require_relative './Item.rb'
+
+
+
+class Book < Item
+  attr_accessor :pages, :author
+
+  def initialize name, price, pages, author
+    super(name, price)
+    @author = author
+    @pages = pages
   end
+
+
 end
