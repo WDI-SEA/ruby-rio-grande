72d760a96d3fcb0ddc0b0891bf8237e9dc5dd097
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-05-03 17:08:18 -0700

    Did test for CD

--- starter-code/spec/cd_spec.rb
+++ starter-code/spec/cd_spec.rb
@@ -5,11 +5,35 @@ describe Cd do
 
   before(:context) do
     #initialize item
+    @cd = Cd.new "CD title", 10.99, 12, "Beiber", "1 Hour"
   end
 
   #check initialization
-  #check that it is an extended from Item
-  #check that it is an instance of Cd
-  #check getters and setters
+  describe "initialization" do
+	  #check that it is an extended from Item
+	  it "extend from the Item class" do
+	  	expect(Cd).to be < Item
+	  end
+	  #check that it is an instance of Cd
+	  it "is an instance of the Cd class" do
+	  	expect(@cd).to be_instance_of(Cd)
+	  end
+	  #check getters and setters
+	  it "is assigned a name" do
+	  	expect(@cd.name).to eq("CD title")
+	  end
+	  it "is assigned a price" do
+	  	expect(@cd.price).to eq(10.99)
+	  end
+	  it "is assigned a tracks" do
+	  	expect(@cd.tracks).to eq(12)
+	  end
+	  it "is assigned a artist" do
+	  	expect(@cd.artist).to eq("Beiber")
+	  end
+	  it "is assigned a run time" do
+	  	expect(@cd.run_time).to eq("1 Hour")
+	  end
+	end
 
 end
