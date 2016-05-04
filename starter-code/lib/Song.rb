effb58e6708d5472e0ac03d1fcec541fc31bbba1
eun-n <eunicena.12@gmail.com>
2016-05-03 17:49:25 -0700

    finished song tests

--- starter-code/lib/Song.rb
+++ starter-code/lib/Song.rb
@@ -1,1 +1,13 @@
 # You are on your own... good luck.
+require_relative 'DigitalItem.rb'
+
+class Song < DigitalItem
+	attr_accessor :runtime
+	attr_accessor :artist
+
+  def initialize name, price, filesize, runtime, artist
+    super(name,price,filesize)
+    @runtime = runtime
+    @artist = artist
+  end
+end
