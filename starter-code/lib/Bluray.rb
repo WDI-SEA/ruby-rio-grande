0fbae667487e93068c81e25b7f4df15972917fe5
bengus101 <bgustafson29@yahoo.com>
2016-05-03 22:56:13 -0700

    added classes to the blu-ray.

--- starter-code/lib/Bluray.rb
+++ starter-code/lib/Bluray.rb
@@ -1,3 +1,11 @@
 require_relative 'Item.rb'
 class Bluray < Item
+	def initialize name, price
+    super(name,price)
+  	end
+
+  	def properties(run_time, director, producer)
+  		@run_time = run_time
+  		@director = director
+  		@producer = producer
 end
