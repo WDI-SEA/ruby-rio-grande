172990bfeafe13e1b8d461a98aa425754f6e819f
mattsluis <Sluismatthew@gmail.com>
2016-05-03 23:31:49 -0700

    msg

--- starter-code/lib/DigitalItem.rb
+++ starter-code/lib/DigitalItem.rb
@@ -3,28 +3,31 @@ require_relative 'Item.rb'
 class Digital_item < Item
 
 
-  attr_accessor :quantity, :description, :weight
+  attr_accessor :quantity, :description 
 
-  def initialize quantity,description
+  def initialize name, price
     super(name,price)
     @quantity = 1
     @description = description
     @weight = -1
-
   end
 
   def sell amount
     puts "item sold"
     @quantity = 1
+    true
   end
 
   def stock amount
     puts "is available"
     @quantity = 1
+    true
   end
 
   def return
     @quantity = 1
     "item returned"
+    true
   end
 end
+
