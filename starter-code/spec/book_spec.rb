f33f345dfe7907ec562d42a59f96a8873d698fb1
montehoover <monte.b.hoover@gmail.com>
2016-05-03 17:01:38 -0700

    finished book tests

--- starter-code/spec/book_spec.rb
+++ starter-code/spec/book_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require_relative '../lib/Item'
 require_relative '../lib/Book'
 
 describe Book do
@@ -9,19 +10,27 @@ describe Book do
 
   #check initialization
   describe "Initialization" do
-    it "is an instance of the Item class" do
+    #check that it is an instance of Book
+    it "is an instance of the Book class" do
       expect(@book).to be_instance_of(Book)
     end
+    #check that it is an extended from Item
+    it "extends from the Item class" do
+      expect(Book).to be < Item
+    end
+    #check getters and setters
     it "is assigned a name" do
       expect(@book.name).to eq("Book Title")
     end
     it "is assigned a price" do
       expect(@book.price).to eq(12.99)
     end
+    it "is assigned pages" do
+      expect(@book.pages).to eq(33)
+    end
+    it "is assigned an author" do
+      expect(@book.author).to eq("Author Name")
+    end
   end
-
-  #check that it is an extended from Item
-  #check that it is an instance of Book
-  #check getters and setters
 
 end
