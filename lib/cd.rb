1ca791e32caf8bf46049124ce74c134f19504933
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-06 15:49:54 -0700

    book and CD tests pass

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,4 +1,13 @@
 require_relative 'item.rb'
 
 class Cd < Item
+  attr_accessor :name, :tracks, :price, :run_time
+
+
+  def initialize(name, tracks, price, run_time)
+    @name = name
+    @tracks = tracks
+    @price = price
+    @run_time = run_time
+  end
 end
