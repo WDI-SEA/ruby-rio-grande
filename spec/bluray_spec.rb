66625fa8367313c6f8943fb05cfe3521996f684a
derek_robertson <dwilliamrobertson@gmail.com>
2016-07-07 09:07:43 -0700

    half way through the challenges

--- spec/bluray_spec.rb
+++ spec/bluray_spec.rb
@@ -1,13 +1,84 @@
 require_relative 'spec_helper'
-require_relative '../src/bluray'
+require './lib/bluray'
 
 describe Bluray do
   before(:context) do
     # initialize item
+    @bluray = Bluray.new('Mad Max: Fury Road', 29.00, '120 min', 'Ridley Scott', 'George Miller')
   end
 
   # check initialization
-  # check that it is an extended from Item
-  # check that it is an instance of Bluray
+  describe 'initialization' do
+    # check that it is an extended from Item
+    it 'should be an item' do
+      expect(@bluray).to be_kind_of(Item)
+    end
+    # check that it is an instance of Bluray
+    it 'should be a bluray' do
+      expect(@bluray).to be_instance_of(Bluray)
+    end
+    # check that it has the correct name
+    it 'is assigned a name' do
+      expect(@bluray.name).to eq('Mad Max: Fury Road')
+    end
+    # check that has price 
+    it 'is assigned a price' do
+      expect(@bluray.price).to eq(29.00)
+    end
+    # check that has run_time
+    it 'is assigned a run_time' do
+      expect(@bluray.run_time).to eq('120 min')
+    end
+    # check that has director
+    it 'is assigned a director' do
+      expect(@bluray.director).to eq('Ridley Scott')
+    end
+    # check that has producer
+    it 'is assigned a producer' do
+      expect(@bluray.producer).to eq('George Miller')
+    end
+  end
   # check getters and setters
+  describe 'getters and setters' do
+    it 'assigns and reads the name' do
+      @cd.name = 'Mad Max: Fury Road'
+      expect(@cd.name).to eq('Mad Max: Fury Road')
+    end
+
+    it 'assigns and reads the price' do
+      @bluray.price = 29.00
+      expect(@bluray.price).to eq(29.00)
+    end
+
+    it 'assigns and reads the run_time' do
+      @bluray.run_time = '120 min'
+      expect(@bluray.run_time).to eq('120 min')
+    end 
+
+    it 'assigns and reads the director' do
+      @bluray.director = 'Ridley Scott'
+    end
+
+    it 'assigns and reads the producer' do
+      @bluray.producer = 'George Miller'
+    end
+  end
+
+  describe 'Methods' do 
+    it "should be able to stock" do
+      result = @bluray.stock 5
+      expect(result).to eq(true)
+      expect(@bluray.quantity).to eq(5)
+    end
+    it 'should not be able to sell more items than are in stock' do
+      result = @bluray.sell 7
+      expect(result).to eq(false)
+      expect(@bluray.quantity).to eq(6)
+    end
+    it 'should be able to sell items and update quantity' do
+      result = @bluray.sell 3
+      expect(result).to eq(true)
+      expect(@bluray.quantity).to eq(2)
+    end
+  end
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -1,13 +1,81 @@
 require_relative 'spec_helper'
-require_relative '../src/cd'
+require './lib/cd'
 
 describe Cd do
   before(:context) do
     # initialize item
+    @cd = Cd.new('Alopecia', 19.99, 14, 'why?', '45:06')
   end
 
   # check initialization
+  describe 'initialization' do
+    # check that it is an instance of Cd
+    it 'should be a cd' do
+      expect(@cd).to be_instance_of(Cd)
+    end
   # check that it is an extended from Item
-  # check that it is an instance of Cd
+    it 'should be an item' do
+      expect(@cd).to be_kind_of(Item)
+    end
+    it 'is assigned a name' do
+      expect(@cd.name).to eq('Alopecia')
+    end
+    it 'is assigned a price' do
+      expect(@cd.price).to eq(19.99)
+    end
+    it 'is assigned a number of tracks' do
+      expect(@cd.tracks).to eq(14)
+    end
+    it 'is assigned an artist' do
+      expect(@cd.artist).to eq('why?')
+    end
+    it 'is assigned a run time' do
+      expect(@cd.run_time).to eq('45:06')
+    end
+  end
   # check getters and setters
+  describe 'getters and setters' do
+    it 'assigns and reads the name' do
+      @cd.name = 'Alopecia'
+      expect(@cd.name).to eq('Alopecia')
+    end
+
+    it 'assigns and reads the price' do
+      @cd.price = 19.99
+      expect(@cd.price).to eq(19.99)
+    end
+
+    it 'assigns and reads the tracks' do
+      @cd.tracks = 14
+      expect(@cd.tracks).to eq(14)
+    end
+
+    it 'assigns and reads the artist' do
+      @cd.artist = 'why?'
+      expect(@cd.artist).to eq('why?')
+    end
+
+    it 'assigns and reads the run_time' do
+      @cd.run_time = '45:06'
+      expect(@cd.run_time).to eq('45:06')
+    end
+  end
+
+  describe "Methods" do
+    it "should be able to stock" do
+      result = @cd.stock 5
+      expect(result).to eq(true)
+      expect(@cd.quantity).to eq(5)
+    end
+    it "should not be able to sell more items than we have" do
+      result = @cd.sell 6
+      expect(result).to eq(false)
+      expect(@cd.quantity).to eq(5)
+    end
+    it "should be able to sell items and update quantity" do
+      result = @cd.sell 3
+      expect(result).to eq(true)
+      expect(@cd.quantity).to eq(2)
+    end
+  end
 end
