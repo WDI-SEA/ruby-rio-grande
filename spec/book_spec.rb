8690e3e9a22c2c29f11ed4f05285a128d110fea7
Ivar Lee <ivarr5@gmail.com>
2016-07-06 19:38:46 -0700

    wrote some tests, learned more about inheritance

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../src/book'
+require_relative '../lib/book'
 
 describe Book do
   before(:context) do
@@ -54,6 +54,24 @@ describe Book do
     it 'assigns and reads the author' do
       @book.author = 'John Milton'
       expect(@book.author).to eq('John Milton')
+    end
+  end
+
+  describe 'Methods' do
+    it 'should be able to stock' do
+      result = @book.stock 5
+      expect(result).to eq(true)
+      expect(@book.quantity).to eq(5)
+    end
+    it 'should not be able to sell more items than we have' do
+      result = @book.sell 6
+      expect(result).to eq(false)
+      expect(@book.quantity).to eq(5)
+    end
+    it 'should be able to sell items and update quantity' do
+      result = @book.sell 3
+      expect(result).to eq(true)
+      expect(@book.quantity).to eq(2)
     end
   end
 end
