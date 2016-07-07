d83b35dee712ed07a0da2f3c84dfdbb42686415c
Krista Calderon <kamsci@gmail.com>
2016-07-06 18:02:16 -0700

    Completed assignment

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,4 +1,14 @@
 require_relative 'item.rb'
 
 class Cd < Item
+
+  attr_accessor :artist, :tracks, :runtime
+
+  def initialize(name, price, tracks, artist, runtime)
+    super(name, price)
+    @tracks = tracks
+    @artist = artist
+    @runtime = runtime
+  end
+
 end
